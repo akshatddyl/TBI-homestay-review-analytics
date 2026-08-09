3f45cc423cd26b24f828471697ca5ef4f241939b
akshatddyl <akshatdhondiyal14@gmail.com>
2026-08-09 22:27:27 +0530

    feat: add frontend shell with brutalism design system

--- server/src/index.js
+++ server/src/index.js
@@ -5,7 +5,7 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const session = require("express-session");
-const MongoStore = require("connect-mongo");
+const MongoStore = require("connect-mongo").default;
 const passport = require("passport");
 
 const connectDB = require("./config/db");
